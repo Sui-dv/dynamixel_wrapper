c745b375510c501109e887007fac3ec2dcc65768
Faiz Pangestu <ffpangestu@gmail.com>
2023-06-20 08:56:01 +0200

    Added wheel_config struct

---
+++ include/dynamixel_wrapper/wheel_config.hpp
@@ -0,0 +1,28 @@
+#ifndef WHEEL_CONFIG_HPP_
+#define WHEEL_CONFIG_HPP_
+
+#include <string>
+#include "dynamixel_wrapper/fake_dynamixel_handle.hpp"
+#include "dynamixel_wrapper/dynamixel_handle.hpp"
+
+using namespace std;
+
+struct WheelConfig 
+{
+  shared_ptr<FakeDynamixelHandle> fake_motor;
+  shared_ptr<DynamixelHandle>     motor;
+
+  bool                            real_hardware = 0;
+  
+  string                          wheel_name = "";
+  int16_t                         wheel_id = -1;
+  
+  double                          encoder_pos = 0;
+  double                          encoder_vel = 0;
+
+  double                          goal = 0;
+
+  double                          radius = 0.05;
+};
+
+#endif // WHEEL_CONFIG_HPP_
